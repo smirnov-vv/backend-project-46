86c475e41d79d27c45867b0b6ad7b2cb0fd48c88
smirnov-vv <vsmirnov.pro@gmail.com>
2023-01-05 16:20:58 +0300

    feat: add arguments and -f option to cli

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,4 +6,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
   .parse()
