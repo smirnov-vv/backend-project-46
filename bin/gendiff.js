5f0dfd31499e3233a07add03288c335095d201d5
smirnov-vv <vsmirnov.pro@gmail.com>
2023-01-06 14:13:14 +0300

    feat: add comparing of two flat objects

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .parse()
+  .action((filepath1, filepath2) => genDiff(filepath1, filepath2))
+  .parse();
